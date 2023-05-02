0277eb423f48808939e8d161fe6d299d3dc36cf5
go-pro0000 <igor.pronin@acgnn.ru>
2023-05-02 07:41:02 +0300

    *

--- packages/client/src/pages/Landing/Landing.tsx
+++ packages/client/src/pages/Landing/Landing.tsx
@@ -1,3 +1,21 @@
+import { Button } from "antd"
+import styles from "./styles.module.scss"
+import "./../../assets/root.css"
+
 export const Landing = () => {
-  return <div>landing</div>
+  return <div className={styles.container}>
+    <h1 className={styles.container__title}>
+      ДОБРО ПОЖАЛОВАТЬ<br />В <span className={styles.container__title__gameName}>AWESOME BATTLE CITY</span>
+    </h1>
+
+    <Button type="primary" size="large" className={styles.container__button}>Играть</Button>
+
+    <h2 className={styles.container__subtitle}>
+      <b>AWESOME BATTLE CITY</b> - невероятно крутой ремейк культовой компьютерной игры <b>BATTLE CITY</b>, выпущенной для игровых приставок Famicom и Game Boy.
+    </h2>
+
+    <h3 className={styles.container__team}>
+      Команда разработки
+    </h3>
+  </div>
 }
