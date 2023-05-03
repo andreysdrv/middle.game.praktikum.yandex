486a4f813e31dea53da0934a3418a5c2a332d725
go-pro0000 <igor.pronin@acgnn.ru>
2023-05-03 23:52:16 +0300

    feature/tgg-13 Вёрстка страниц ошибок

--- packages/client/src/pages/NotFound/NotFound.tsx
+++ packages/client/src/pages/NotFound/NotFound.tsx
@@ -1,3 +1,15 @@
+import { Link } from 'react-router-dom'
+import styles from "../ServerError/styles.module.scss"
+import "./../../assets/root.css"
+
 export const NotFound = () => {
-  return <div>NotFound</div>
+  return <div className={styles.container}>
+  <img src="/public/notFoundPage/notFound.svg" alt="" className={styles.container__image} />
+
+  <h1 className={styles.container__title}>Не туда попали</h1>
+
+  <Link className={styles.container__link} to="/">
+    На главную
+  </Link>
+</div>
 }
--- packages/client/src/pages/ServerError/ServerError.tsx
+++ packages/client/src/pages/ServerError/ServerError.tsx
@@ -1,3 +1,15 @@
+import { Link } from 'react-router-dom'
+import styles from "./styles.module.scss"
+import "./../../assets/root.css"
+
 export const ServerError = () => {
-  return <div>ServerError</div>
+  return <div className={styles.container}>
+    <img src="/public/serverErrorPage/serverError.svg" alt="" className={styles.container__image} />
+
+    <h1 className={styles.container__title}>Что-то пошло не так :(</h1>
+
+    <Link className={styles.container__link} to="/">
+      На главную
+    </Link>
+  </div>
 }
