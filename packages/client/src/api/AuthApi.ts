03f394aec2f667d8058ba17e11284e3595b26f2a
Кирилл Зикевский <2309696@mail.ru>
2023-05-03 17:04:02 +0300

    fix: first review comments done

--- packages/client/src/api/AuthApi.ts
+++ packages/client/src/api/AuthApi.ts
@@ -1,4 +1,4 @@
-import { baseUrl } from '../utils/constants';
+import { BASE_URL } from '../utils/constants'
 
 export interface SignupData {
   first_name: string
@@ -15,45 +15,27 @@ interface SigninData {
   password: string
 }
 
-// interface User {
-//   id: number;
-//   first_name: string;
-//   second_name: string;
-//   login: string;
-//   email: string;
-//   password: string;
-//   phone: string;
-//   avatar: string;
-// }
-
 class AuthApi {
-
   private readonly baseUrl: string
 
   constructor(baseUrl: string) {
-    this.baseUrl = baseUrl;
+    this.baseUrl = baseUrl
   }
 
   private checkResponse(response: Response) {
     if (response.ok) {
-      return response.json();
+      return response.json()
     }
-    return Promise.reject(response.status);
+    return Promise.reject(response.status)
   }
 
   public signup(signupData: SignupData) {
-    const { 
-      first_name,
-      second_name,
-      login,
-      email,
-      password,
-      phone
-    } = signupData;
+    const { first_name, second_name, login, email, password, phone } =
+      signupData
     return fetch(`${this.baseUrl}/auth/signup`, {
       method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         first_name: first_name,
@@ -61,38 +43,34 @@ class AuthApi {
         login: login,
         email: email,
         password: password,
-        phone: phone
+        phone: phone,
       }),
-    })
-    .then(this.checkResponse);
+    }).then(this.checkResponse)
   }
 
   public signin(signinData: SigninData) {
-    const { login, password } = signinData;
+    const { login, password } = signinData
     return fetch(`${this.baseUrl}/auth/signin`, {
       method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         login: login,
         password: password,
       }),
-    })
-    .then(this.checkResponse);
+    }).then(this.checkResponse)
   }
 
   public fetchUser() {
     return fetch(`${this.baseUrl}/auth/user`, {
       method: 'GET',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-      credentials: 'include'
-    })
-    .then(this.checkResponse);
+      credentials: 'include',
+    }).then(this.checkResponse)
   }
-
 }
 
-export default new AuthApi(baseUrl);
+export default new AuthApi(BASE_URL)
--- packages/client/src/pages/Login/Login.tsx
+++ packages/client/src/pages/Login/Login.tsx
@@ -1,173 +1,159 @@
-import React, { useEffect, useState } from 'react';
-import AuthApi from '../../api/AuthApi';
-import { Link, useNavigate } from 'react-router-dom';
-import { Form, Input, Button, Typography, message } from 'antd';
-import styles from './login.module.css';
-
-
+import React, { useState } from 'react'
+import AuthApi from '../../api/AuthApi'
+import { Link, useNavigate } from 'react-router-dom'
+import { Form, Input, Button, Typography, message } from 'antd'
+import styles from './login.module.css'
 
 const Login = () => {
-    const navigate = useNavigate();
-    const [ form ] = Form.useForm();
-    const [ isLoading, setIsLoading ] = useState<boolean>(false);
+  const navigate = useNavigate()
+  const [form] = Form.useForm()
+  const [isLoading, setIsLoading] = useState(false)
 
-    useEffect(() => {
-        return () => {
-            console.log('component will unmount')
+  const handleSubmit = (data: { login: string; password: string }) => {
+    setIsLoading(true)
+    AuthApi.signin(data)
+      .then(() => {
+        message.success('Авторизация прошла успешно', 3)
+        setTimeout(() => {
+          AuthApi.fetchUser()
+          navigate('/game')
+        }, 1000)
+      })
+      .catch(e => {
+        if (e === 400) {
+          message.error('Неверный запрос', 3)
+        } else if (e === 401) {
+          message.error('Неверный логин или пароль', 3)
+        } else if (e === 500) {
+          message.error('Произошла ошибка на сервере', 3)
         }
-    }, []);
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }
 
-    const handleSubmit = (data: { login: string, password: string }) => {
-        setIsLoading(true);
-        AuthApi.signin(data)
-            .then(() => {
-                message.success('Авторизация прошла успешно', 3);
-                setTimeout(() => {
-                    AuthApi.fetchUser();
-                    navigate('/game');
-                  }, 1000);
-            })
-            .catch((e) => {
-                if (e === 400) {
-                    message.error('Неверный запрос', 3);
-                } else if (e === 401) {
-                    message.error('Неверный логин или пароль', 3);
-                } else if (e === 500) {
-                    message.error('Произошла ошибка на сервере', 3);
-                }
-            })
-            .finally(() => {
-                setIsLoading(false);
-            });
-    }
+  return (
+    <main className={styles.main}>
+      <Form
+        form={form}
+        name="login"
+        size="large"
+        className={styles.form}
+        onFinish={() => {
+          handleSubmit(form.getFieldsValue())
+        }}>
+        <Typography.Title
+          level={1}
+          // Если записать эти стили в className, то они не применятся (не перезапишутся)
+          style={{
+            fontSize: '32px',
+            fontWeight: '500',
+          }}
+          className={styles.title}>
+          Вход
+        </Typography.Title>
 
-    return (
-            <main className={styles.main}>
-                <Form
-                    form={form}
-                    name='login'
-                    size='large'
-                    className={styles.form}
-                    onFinish={
-                        () => {
-                            handleSubmit(form.getFieldsValue())
-                        }
-                    }
-                >
-                    <Typography.Title
-                        level={1}
-                        style={{
-                            fontSize: '32px',
-                            fontWeight: '500'
-                        }}
-                        className={styles.title}
-                    >
-                        Вход
-                    </Typography.Title>
+        <Form.Item
+          name="login"
+          validateFirst={true}
+          rules={[
+            {
+              required: true,
+              message: 'Введите логин',
+            },
+            {
+              whitespace: true,
+              message: 'Поле не может быть пустым',
+            },
+            {
+              min: 3,
+              message: 'Введите минимум 3 символа',
+            },
+            {
+              max: 16,
+              message: 'Введите не более 16 символов',
+            },
+            () => ({
+              validator(_, value) {
+                // Если перенести решулярку в отдельную константу в utils, то валидация не отрабатывает
+                // как требуется.
+                return /^(?![\W_|-]{1})(?!.*_-)(?!.*-_)(?!.*--)(?!.*__).[\w-]*(?<!.-|_)$/gi.test(
+                  value
+                )
+                  ? Promise.resolve()
+                  : Promise.reject('Непрвильный формат логина')
+              },
+            }),
+          ]}
+          hasFeedback>
+          <Input placeholder="Логин" style={{ fontSize: '16px' }} />
+        </Form.Item>
 
-                    <Form.Item
-                        name="login"
-                        validateFirst={true}
-                        rules={[
-                            { 
-                                required: true, 
-                                message: 'Введите логин' 
-                            },
-                            { 
-                                whitespace: true,
-                                message: 'Поле не может быть пустым'
-                            },
-                            { 
-                                min: 3,
-                                message: 'Введите минимум 3 символа'
-                            },
-                            { 
-                                max: 16,
-                                message: 'Введите не более 16 символов'
-                            },
-                            () => ({
-                                validator(_, value) {
-                                   return /^(?![\W_|-]{1})(?!.*_-)(?!.*-_)(?!.*--)(?!.*__).[\w-]*(?<!.-|_)$/gi.test(value) ? 
-                                    Promise.resolve()
-                                    : 
-                                    Promise.reject('Непрвильный формат логина')
-                                }
-                            })
-                        ]}
-                        hasFeedback
-                    >
-                        <Input 
-                            placeholder="Логин"
-                            style={{fontSize: '16px'}}
-                        />
-                    </Form.Item>
+        <Form.Item
+          name="password"
+          style={{ fontSize: '10px' }}
+          validateFirst={true}
+          rules={[
+            {
+              required: true,
+              message: 'Введите пароль',
+            },
+            {
+              whitespace: true,
+              message: 'Поле не может быть пустым',
+            },
+            {
+              min: 8,
+              message: 'Введите минимум 8 символов',
+            },
+            {
+              max: 40,
+              message: 'Введите не более 40 символов',
+            },
+            () => ({
+              validator(_, value) {
+                // Если перенести решулярку в отдельную константу в utils, то валидация не отрабатывает
+                // как требуется.
+                return /^(?=.*?[0-9])(?=.*?[A-Z]).*$/.test(value)
+                  ? Promise.resolve()
+                  : Promise.reject('Непрвильный формат пароля')
+              },
+            }),
+          ]}
+          hasFeedback>
+          <Input.Password
+            type="password"
+            placeholder="Пароль"
+            style={{ fontSize: '16px' }}
+          />
+        </Form.Item>
 
-                    <Form.Item
-                        name="password"
-                        style={{fontSize: '10px'}}
-                        validateFirst={true}
-                        rules={[
-                            {
-                                required: true, 
-                                message: 'Введите пароль'
-                            },
-                            { 
-                                whitespace: true,
-                                message: 'Поле не может быть пустым'
-                            },
-                            { 
-                                min: 8,
-                                message: 'Введите минимум 8 символов'
-                            },
-                            { 
-                                max: 40,
-                                message: 'Введите не более 40 символов'
-                            },
-                            () => ({
-                                validator(_, value) {
-                                   return /^(?=.*?[0-9])(?=.*?[A-Z]).*$/.test(value) ? 
-                                    Promise.resolve()
-                                    : 
-                                    Promise.reject('Непрвильный формат пароля')
-                                }
-                            })
-                        ]}
-                        hasFeedback
-                    >
-                        <Input.Password
-                            type="password"
-                            placeholder="Пароль"
-                            style={{fontSize: '16px'}}
-                        />
-                    </Form.Item>
-
-                    <Form.Item
-                        shouldUpdate
-                    >
-                        {() => (
-                            <>
-                            <Button
-                                className={styles.button}
-                                block
-                                type="primary"
-                                htmlType="submit"
-                                loading={isLoading}
-                                disabled={
-                                    !form.isFieldsTouched(true) ||
-                                    form.getFieldsError().filter(({ errors }) => errors.length)
-                                    .length > 0
-                                }
-                            >
-                                Войти
-                            </Button>
-                            <Link className={styles.link} to='/register'>Еще не зарегистрированы?</Link>
-                            </>
-                        )}
-                    </Form.Item>
-                </Form>
-            </main>
-
-    );
+        <Form.Item shouldUpdate>
+          {() => (
+            <>
+              <Button
+                className={styles.button}
+                block
+                type="primary"
+                htmlType="submit"
+                loading={isLoading}
+                disabled={
+                  !form.isFieldsTouched(true) ||
+                  form.getFieldsError().filter(({ errors }) => errors.length)
+                    .length > 0
+                }>
+                Войти
+              </Button>
+              <Link className={styles.link} to="/register">
+                Еще не зарегистрированы?
+              </Link>
+            </>
+          )}
+        </Form.Item>
+      </Form>
+    </main>
+  )
 }
 
-export { Login };
+export { Login }
