09f85f16f61b8ef07a9945625ab2f496a02d4d05
go-pro0000 <igor.pronin@acgnn.ru>
2023-05-06 21:01:55 +0300

    fix: исправление ошибок автотестов

--- packages/client/src/components/UI/Avatar/Avatar.tsx
+++ packages/client/src/components/UI/Avatar/Avatar.tsx
@@ -2,7 +2,12 @@ import { Avatar } from "antd"
 import styles from "./styles.module.scss"
 import "./../../../assets/root.css"
 
-export const UserCircle = ({name, src}) => {
+export interface UserCircleProps {
+    name: string,
+    src: string,
+}
+
+export const UserCircle = ({name, src}: UserCircleProps) => {
    return <div className={styles.container}>
        <Avatar className={styles.container__avatar} src={src}>User</Avatar>
        <span className={styles.container__text}>{name}</span>
