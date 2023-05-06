cf65b6371f0fb1e3008316fe359345658d2f0081
go-pro0000 <igor.pronin@acgnn.ru>
2023-05-06 22:37:42 +0300

    feat: отцентровал картинку

--- packages/client/src/pages/ServerError/ServerError.tsx
+++ packages/client/src/pages/ServerError/ServerError.tsx
@@ -1,15 +1,18 @@
 import { Link } from 'react-router-dom'
 import styles from "./styles.module.scss"
-import "./../../assets/root.css"
 
 export const ServerError = () => {
-  return <div className={styles.container}>
-    <img src="/public/serverErrorPage/serverError.svg" alt="" className={styles.container__image} />
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <img src="/public/serverErrorPage/serverError.svg" alt="" className={styles.container__image} />
 
-    <h1 className={styles.container__title}>Что-то пошло не так :(</h1>
+        <h1 className={styles.container__title}>Что-то пошло не так :(</h1>
 
-    <Link className={styles.container__link} to="/">
-      На главную
-    </Link>
-  </div>
+        <Link className={styles.container__link} to="/">
+          На главную
+        </Link>
+      </div>
+    </div>
+  )
 }
