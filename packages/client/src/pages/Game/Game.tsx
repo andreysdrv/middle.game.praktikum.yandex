605ec46170e9ddc28f7d65724eb67037aecafcc8
go-pro0000 <igor.pronin@acgnn.ru>
2023-05-06 21:33:47 +0300

    feature/tgg-23 Вёрстка экрана игры Завершение

--- packages/client/src/pages/Game/Game.tsx
+++ packages/client/src/pages/Game/Game.tsx
@@ -4,15 +4,33 @@ import styles from "./styles.module.scss"
 
 export const Game = () => {
   const [buttonsList] = useState<Array<string>>(["Одиночная игра", "Сетевая игра", "Параметры"]);
-  const [gameMode] = useState<string>("gameStart");
+  const [buttonsList2] = useState<Array<string>>(["Eщё раз", "Главное меню"]);
+
+  const [gameMode] = useState<string>("gameEnd");
 
   return (
     <section>
-      { gameMode === 'gameStart' &&
+      {gameMode === 'gameStart' &&
         <div className={styles.container}>
           <h1 className={styles.container__title}>AWESOME BATTLE CITY</h1>
 
           {buttonsList.map((item, index) => {
+            return <div key={index}>
+              <Button type="primary" className={styles.container__button}>{item}</Button>
+            </div>
+          })}
+        </div>
+      }
+
+      {gameMode === 'gameEnd' &&
+        <div className={styles.container}>
+          <h1 className={styles.container__title}>AWESOME BATTLE CITY</h1>
+
+          <h2 className={styles.container__subtitle}>Игра окончена</h2>
+
+          <p className={styles.container__score}>Счёт: <span>770</span></p>
+
+          {buttonsList2.map((item, index) => {
             return <div key={index}>
               <Button type="primary" className={styles.container__button}>{item}</Button>
             </div>
