8c0e806af1bd33a63172e7d1184a26ade8e1fe30
Кирилл Зикевский <2309696@mail.ru>
2023-05-07 16:18:12 +0300

    refactor: revire commments accomplished

--- packages/client/src/layouts/Main/Main.tsx
+++ packages/client/src/layouts/Main/Main.tsx
@@ -1,16 +1,11 @@
 import { Header } from '../../components/Header'
 import { Outlet } from 'react-router-dom'
-import ErrorBoundary from '../../components/ErrorBoudary/ErrorBoundary'
 
 export const Main = () => {
   return (
     <>
-      <ErrorBoundary>
         <Header />
-      </ErrorBoundary>
-      <ErrorBoundary>
         <Outlet />
-      </ErrorBoundary>
     </>
   )
 }
