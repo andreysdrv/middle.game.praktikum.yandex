7f97602cb5543617809e8f197dd9cf48ffe5d52d
Nikita <n173@bk.ru>
2023-05-10 21:28:16 +0300

    исправления по итогам ревью от Вячеслава

--- packages/client/src/pages/Profile/const.tsx
+++ packages/client/src/pages/Profile/const.tsx
@@ -1,47 +1,36 @@
-export type UserProps = {
-    userId: number;
-    avatar: string;
-    email: string;
-    login: string;
-    display_name: string;
-    first_name: string; 
-    phone: string;
+import { UserProps, UserStatProps } from "./types";
+
+export const USER_DATA: UserProps = {
+  userId: 1,
+  avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7TjyxXhIkwiNE1996F349Lr8xRDEyQ7r6cA&usqp=CAU",
+  email: "email@SizeContext.com",
+  login: "UserLogin",
+  display_name: "UserNickname",
+  first_name: "Nik",
+  phone: "+79999999999"
 };
 
-export type UserStatProps = {
-    userId: number;
-    totalScore: number;
-    bestGame: number;
-    winnerCount: number;
-    defeatCount: number;
-    winRate: number;
-};
-
-export type FileProps = {
-    fileList: unknown,
-    file: {
-        name: unknown,
-        status: unknown,
-    }
+export const USER_STAT_DATA: UserStatProps = {
+  userId: 1,
+  totalScore: 50,
+  bestGame: 552,
+  winnerCount: 10,
+  defeatCount: 55,
+  winRate: 72
 }
 
-
-
-export const userData: UserProps = {
-    userId: 1,
-    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7TjyxXhIkwiNE1996F349Lr8xRDEyQ7r6cA&usqp=CAU",
-    email: "email@SizeContext.com",
-    login: "UserLogin",
-    display_name: "UserNickname",
-    first_name: "Nik",
-    phone: "+79999999999"
+export const LAYOUT = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
 };
 
-export const userStatData: UserStatProps = {
-    userId: 1,
-    totalScore: 50,
-    bestGame: 552,
-    winnerCount: 10,
-    defeatCount: 55,
-    winRate: 72
-}
+export const TAIL_LAYOUT = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
