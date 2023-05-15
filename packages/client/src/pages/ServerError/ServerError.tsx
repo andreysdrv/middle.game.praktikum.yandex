060738475bbf441606febfd24d288e4af2d0ba20
andreysdrv <sidorov.au.official@gmail.com>
2023-05-15 15:14:55 +0300

    style: форматирование кода

--- packages/client/src/pages/ServerError/ServerError.tsx
+++ packages/client/src/pages/ServerError/ServerError.tsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom'
-import styles from "./styles.module.scss"
+import styles from './styles.module.scss'
 
 export const ServerError = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <img src="/public/serverErrorPage/serverError.svg" alt="" className={styles.container__image} />
+        <img
+          src="/public/serverErrorPage/serverError.svg"
+          alt=""
+          className={styles.container__image}
+        />
 
         <h1 className={styles.container__title}>Что-то пошло не так :(</h1>
 
